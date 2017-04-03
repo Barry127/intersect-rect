5ed6c83a9620dc600f8e6c393326ee7db5481047
Barry de Kleijn <kleijn.barry@gmail.com>
2017-04-03 20:37:00 +0200

    initial files

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "xo-space",
+  "rules": {
+    "object-curly-spacing": [
+      2,
+      "always"
+    ],
+    "space-before-function-paren": [
+      2,
+      {
+        "anonymous": "always",
+        "named": "always"
+      }
+    ]
+  }
+};
